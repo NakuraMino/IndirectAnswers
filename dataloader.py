e1e06f717afc470dedc9f4c9f92f189b430a31e5
nakuram <minorunakura@gmail.com>
2021-02-20 15:26:06 -0800

    fixed dataloader tokenizing stuff

--- dataloader.py
+++ dataloader.py
@@ -19,7 +19,7 @@ class CircaDataset(Dataset):
     dataset class for the circa dataset 
     """
 
-    def __init__(self, file_path, tokenizer=None):
+    def __init__(self, file_path, tokenizer=None, use_tokenizer=False):
         """
         @param file_path: the path to a circa.tsv file
         @param tokenizer (default=None): an optional tokenizer to convert text 
@@ -28,6 +28,7 @@ class CircaDataset(Dataset):
         """
         self.file_path = file_path
         self.data = pd.read_csv(file_path, sep='\t')
+        self.use_tokenizer = use_tokenizer
         if tokenizer == None:
             self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         else:
@@ -36,30 +37,54 @@ class CircaDataset(Dataset):
     def __len__(self):
         return self.data.shape[0]
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx): 
         """
         @param idx: index into the dataset
 
         @return: dictionary mapping from th column titles to the value at each column + row(idx) pair
         """
-        header_to_data = dict()
-        for header in self.data.columns:
-            if header == "id":
-                data = self.data.loc[idx][header]                                
-            elif header == "judgements" or header == "goldstandard1" or header == "goldstandard2":
-                # data labels
-                data = self.data.loc[idx][header]
-            else: 
-                # context, question-X, canquestion-X, answer-Y
-                text = str(self.data.loc[idx][header]) 
-                tokenized_text = self.tokenizer.tokenize(text)
-                indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
-                data = indexed_tokens
-            header_to_data[header] = data
-        return header_to_data
+        header_to_data = dict() 
+        judgement = self.data.loc[idx]["judgements"]
+        label1 = self.labelToIdx(self.data.loc[idx]["goldstandard1"])
+        label2 = self.labelToIdx(self.data.loc[idx]["goldstandard2"])
+        question = str(self.data.loc[idx]["context"]) + \
+                   "[SEP] " + str(self.data.loc[idx]['question-X']) + \
+                   "[SEP] " + str(self.data.loc[idx]['answer-Y'])
+        indexed_tokens = self.tokenizer(question, return_tensors="pt")
+        return {
+            "judgements": judgement,
+            "goldstandard1": label1,
+            "goldstandard2": label2,
+            "question": indexed_tokens,
+        }
 
+    def labelToIdx(self, label):
+        """
+        maps a label from the circa dataset to a numerical value 
+        
+        @param label: the gold standard label
+        @return: a value 1 - 8 which encodes the label
+        """
+        if self.use_tokenizer: 
+            return self.tokenizer(label, return_tensors="pt")
+        if label == 'Yes':
+            return 1
+        elif label == 'No':
+            return 2
+        elif label == 'Yes, subject to some conditions':
+            return 3
+        elif label == "In the middle, neither yes nor no":
+            return 4
+        elif label == "Other":
+            return 5
+        elif label == 'Probably yes / sometimes yes':
+            return 6
+        elif label == "Probably no":
+            return 7
+        elif label == "I am not sure how X will interpret Y's answer":
+            return 8
 
-def getCircaDataloader(file_path, batch_size=16, num_workers=4, shuffle=True):
+def getCircaDataloader(file_path, batch_size=16, num_workers=4, shuffle=True, use_tokenizer=False):
     """
     creates a dataset and returns a dataloader 
 
@@ -69,7 +94,7 @@ def getCircaDataloader(file_path, batch_size=16, num_workers=4, shuffle=True):
     @param shuffle (default=True): shuffle dataset or not (True or False value)
     @return: torch.utils.data.DataLoader object    
     """
-    dataset = CircaDataset(file_path)
+    dataset = CircaDataset(file_path, use_tokenizer=use_tokenizer)
     return DataLoader(dataset,
                       batch_size=batch_size,
                       shuffle=shuffle,
@@ -77,11 +102,33 @@ def getCircaDataloader(file_path, batch_size=16, num_workers=4, shuffle=True):
 
 if __name__ == "__main__":
     # some really simple testing 
-
     dataset = CircaDataset('./data/circa-data.tsv')
     length = len(dataset)
     print(length)
     print(dataset[0])
-    dataloader = getCircaDataloader('./data/circa-data.tsv', num_workers=1)
+    dataloader = getCircaDataloader('./data/circa-data.tsv', batch_size=1, num_workers=1)
     dl_iter = iter(dataloader)
     print(next(dl_iter))
+
+# def __getitem__(self, idx):
+#     """
+#     @param idx: index into the dataset
+
+#     @return: dictionary mapping from th column titles to the value at each column + row(idx) pair
+#     """
+#     header_to_data = dict()
+#     for header in self.data.columns:
+#         if header == "id":
+#             data = self.data.loc[idx][header]                                
+#         elif header == "judgements" or header == "goldstandard1" or header == "goldstandard2":
+#             # data labels
+#             data = self.data.loc[idx][header]
+#         else: 
+#             # context, question-X, canquestion-X, answer-Y
+#             text = str(self.data.loc[idx][header]) 
+#             indexed_tokens = self.tokenizer(text, return_tensors="pt")
+#             # tokenized_text = self.tokenizer.tokenize(text)
+#             # indexed_tokens = torch.tensor(self.tokenizer.convert_tokens_to_ids(tokenized_text))
+#             data = indexed_tokens
+#         header_to_data[header] = data
+#     return header_to_data

82f40c77cca78f82ce71a3f99f0ce2814eafd2fd
Andressa Chan <ac426@attu1.cs.washington.edu>
2021-02-22 11:01:51 -0800

    fix testing

--- testing.py
+++ testing.py
@@ -1,22 +1,17 @@
+import sys
 import torch
 from torch import nn
 import torch.nn.functional as F
 from tqdm import tqdm
+from transformers import BertForSequenceClassification
 from sklearn.metrics import accuracy_score
 
-def train(model, dataloader, optimizer, device, pretrained_bool):
-    for texts, labels in tqdm(dataloader):
-        texts, labels = texts.to(device), labels.to(device)
-        output = model(texts)
-        loss = F.cross_entropy(output, labels)
-        model.zero_grad()
-        loss.backward()
-        optimizer.step()
+from datasets import load_metric
+metric = load_metric("accuracy")
 
 
-def evaluate(model, dataloader, device):
+def eval(model, dataloader, device):
     count = correct = 0.0
-    f1 = None
     actual = []
     pred = []
     with torch.no_grad():
@@ -26,11 +21,18 @@ def evaluate(model, dataloader, device):
             output = model(texts)
             # shape: (batch_size,)
             predicted = output.argmax(dim=-1)
-            count += len(predicted)
-            correct += (predicted == labels).sum().item()
             pred.extend([val.item() for val in predicted])
             actual.extend([val.item() for val in labels])
     accuracy = accuracy_score(actual, pred)
     print(f"Accuracy: {accuracy}")
     return accuracy
 
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python testing.py modelPath dataloader")
+    path = sys.argv[1]
+    dataloader = sys.argv[2]
+    model = BertForSequenceClassification()
+    model.load_state_dict(torch.load(path))
+
+    eval(model, dataloader, device)

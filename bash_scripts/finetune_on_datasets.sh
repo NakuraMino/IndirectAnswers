3503093449536f6bfc5c3acedcdc55e222a952d5
Millicent Li <mllcntl@gmail.com>
2021-02-21 16:45:30 -0800

    holding for merge

--- bash_scripts/finetune_on_datasets.sh
+++ bash_scripts/finetune_on_datasets.sh
@@ -2,5 +2,18 @@
 
 ## File for finetuning on the datasets
 
-BASE_DIR=""
-OUTPUT_DIR=""
+BASE_DIR="/data2/limill01/IndirectAnswers"
+OUTPUT_DIR="/data2/limill01/IndirectAnswers/results"
+MODEL_TYPE="bert-base-cased"
+
+cd "$BASE_DIR"
+
+## For label of size 9
+echo "Finetuning on CIRCA..."
+python finetuning.py \
+    --train_data "$BASE_DIR/data/circa-data-train.tsv" \
+    --dev_data "$BASE_DIR/data/circa-data-dev.tsv" \
+    --test_data "$BASE_DIR/data/circa-data-test.tsv" \
+    --model_type "$MODEL_TYPE" \
+    --num_labels "9"
+    

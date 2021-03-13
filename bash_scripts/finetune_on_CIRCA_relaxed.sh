a68096db9b03a8ca0c6b2290f6a418380aab0532
Andressa Chan <ac426@attu8.cs.washington.edu>
2021-03-12 18:17:01 -0800

    holding for merge

--- bash_scripts/finetune_on_CIRCA_relaxed.sh
+++ bash_scripts/finetune_on_CIRCA_relaxed.sh
@@ -1,39 +1,36 @@
 #!/bin/bash
 
-## File for finetuning on the relaxed dataset
+## File for finetuning on the matched, relaxed dataset
 
-BASE_DIR="/data2/limill01/IndirectAnswers/"
-OUTPUT_DIR="/data2/limill01/IndirectAnswers/results/"
+BASE_DIR=".."  # IndirectAnswers directory
+OUTPUT_DIR="$BASE_DIR/results/"
 MODEL_TYPE="bert-base-cased"
 
 cd "$BASE_DIR"
 
-LearningRates="5e-5 3e-5 2e-5"
-Epochs="2 3 4"
-BatchSize="16 32"
+Modes=('q' 'a' 'qa') # q=question, a=answer
+LearningRates=('3e-5' '2e-5' '3e-5')
 
 ## Modify the script so that we can make a new directory for the different BERT experiments
 ## For label of size 6
-echo "Finetuning on CIRCA with relaxed labels..."
+echo "Finetuning on CIRCA with matched, relaxed labels..."
 
-# Running through the different combinations
-for lr in $LearningRates; do
-    for e in $Epochs; do
-        for b in $BatchSize; do
-            echo "lr: $lr, epoch: $e, batch: $b"
-            python finetuning.py \
-                --train_data "$BASE_DIR/data/circa-data-train.tsv" \
-                --dev_data "$BASE_DIR/data/circa-data-dev.tsv" \
-                --model_name "CIRCA_BERT_relaxed_e${e}_lr${lr}_b${b}" \
-                --dataset_type "1" \
-                --batch_size "$b" \
-                --epochs "$e" \
-                --learning_rate "$lr" \
-                --output_dir "$OUTPUT_DIR" \
-                --model_type "$MODEL_TYPE" \
-                --num_labels "6"
-        done
-    done
+# Running through the different modes, using their best hyperparameters
+for i in ${!Modes[@]}; do
+    mode=${Modes[i]}
+    lr=${LearningRates[i]}
+    echo "STARTING... Mode: $mode, Learning rate: $lr"
+    python finetuning.py \
+        --train_data "$BASE_DIR/data/circa-data-train.tsv" \
+        --dev_data "$BASE_DIR/data/circa-data-dev.tsv" \
+        --model_name "CIRCA_BERT_matched_relaxed_e3_lr${lr}_b32" \
+        --dataset_type "CIRCA" \
+        --batch_size "32" \
+        --epochs "3" \
+        --learning_rate "$lr" \
+        --output_dir "$OUTPUT_DIR" \
+        --model_type "$MODEL_TYPE" \
+        --num_labels "6"
 done
 
-echo "Finished finetuning CIRCA with relaxed labels..."
+echo "Finished finetuning CIRCA with matched, relaxed labels..."

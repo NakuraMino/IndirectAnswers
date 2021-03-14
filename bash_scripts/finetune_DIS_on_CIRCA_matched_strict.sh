f9e348f62cf2d78574497845105f95b72c7b73df
Ubuntu <azureuser@nlp.bfq2lfmzdryu1ooa22qyyhquxg.bx.internal.cloudapp.net>
2021-03-14 23:19:23 +0000

    added bash scripts for running DIS CIRCa

---
+++ bash_scripts/finetune_DIS_on_CIRCA_matched_strict.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## File for finetuning the DIS model on the matched, strict version of the CIRCA dataset
+
+#BASE_DIR=".."  # IndirectAnswers directory
+#BASE_DIR="/data2/limill01/IndirectAnswers"
+BASE_DIR="/home/azureuser/IndirectAnswers"
+OUTPUT_DIR="$BASE_DIR/models/"
+MODEL_TYPE="/home/azureuser/IndirectAnswers/models/DIS_BERT_e3_lr2e-5_b16"
+
+cd "$BASE_DIR"
+
+
+## Modify the script so that we can make a new directory for the different BERT experiments
+## For label of size 9
+echo "Finetuning DIS CIRCA with matched, strict labels..."
+
+# Running through the different modes, using their best hyperparameters
+lr="3e-5"
+echo "STARTING... Learning rate: $lr"
+python finetuning.py \
+    --train_data "$BASE_DIR/data/CIRCA/circa-data-train.tsv" \
+    --dev_data "$BASE_DIR/data/CIRCA/circa-data-dev.tsv" \
+    --model_name "DIS_CIRCA_BERT_matched_strict_e3_lr${lr}_b32" \
+    --dataset_type "CIRCA" \
+    --multi_gpu_on \
+    --transfer_on \
+    --batch_size "32" \
+    --epochs "3" \
+    --learning_rate "$lr" \
+    --output_dir "$OUTPUT_DIR" \
+    --model_type "$MODEL_TYPE" \
+    --num_labels "9" \
+    --prev_labels "5"
+
+echo "Finished finetuning DIS CIRCA with matched, strict labels..."

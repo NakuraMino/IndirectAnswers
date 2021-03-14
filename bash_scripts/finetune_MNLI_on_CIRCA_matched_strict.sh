17b600e94488dd563a1b8aa2bd56715aef38a4a0
Ubuntu <azureuser@nlp.4spx0bfg1m4e5jqucncun521zb.bx.internal.cloudapp.net>
2021-03-14 23:18:08 +0000

    strict

---
+++ bash_scripts/finetune_MNLI_on_CIRCA_matched_strict.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## File for finetuning the MNLI model on the matched, relaxed version of the CIRCA dataset
+
+#BASE_DIR=".."  # IndirectAnswers directory
+#BASE_DIR="/data2/limill01/IndirectAnswers"
+BASE_DIR="/home/azureuser/IndirectAnswers"
+OUTPUT_DIR="$BASE_DIR/models/"
+MODEL_TYPE="/home/azureuser/IndirectAnswers/models/MNLI_BERT_e3_lr2e-5_b16"
+
+cd "$BASE_DIR"
+
+## Modify the script so that we can make a new directory for the different BERT experiments
+## For label of size 6
+echo "Finetuning on CIRCA with matched, strict labels..."
+
+# Running through the different modes, using their best hyperparameters
+lr="2e-5"
+echo "STARTING... Learning rate: $lr"
+python finetuning.py \
+    --train_data "$BASE_DIR/data/CIRCA/circa-data-train.tsv" \
+    --dev_data "$BASE_DIR/data/CIRCA/circa-data-dev.tsv" \
+    --model_name "MNLI_CIRCA_BERT_matched_strict_e3_lr${lr}_b32" \
+    --dataset_type "CIRCA" \
+    --multi_gpu_on \
+    --transfer_on \
+    --batch_size "32" \
+    --epochs "3" \
+    --learning_rate "${lr}" \
+    --output_dir "$OUTPUT_DIR" \
+    --model_type "$MODEL_TYPE" \
+    --num_labels "9" \
+    --prev_labels "4"
+
+echo "Finished finetuning MNLI CIRCA with matched, strict labels..."

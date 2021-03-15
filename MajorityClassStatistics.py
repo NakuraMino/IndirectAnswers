a5f7c16b6660d2b5186bd704d608025f638075cb
nakuram <minorunakura@gmail.com>
2021-03-14 18:04:11 -0700

    finished majority class script

--- MajorityClassStatistics.py
+++ MajorityClassStatistics.py
@@ -6,6 +6,7 @@ including F1 scores and accuracies
 import sklearn.metrics as metrics
 import dataloader 
 import torch 
+import statistics 
 
 def getClassLabels(file_path):
     """
@@ -36,13 +37,15 @@ def getClassLabels(file_path):
             relaxed_labels = torch.cat([relaxed_labels, relaxed_batch_labels], dim=0)
     return strict_labels, relaxed_labels
 
-def printStatistics(pred_labels, y_labels, title=""):
+def printStatistics(pred_labels, y_labels, title="", p=True):
     f1_score = metrics.f1_score(y_labels, pred_labels, average=None)
     num_correct = torch.sum(pred_labels == y_labels)
     average = num_correct / y_labels.shape[0]
-    print(f"{title} accuracy: {average}")
-    print(f"{title} f1 scores: {f1_score}")
-    print()
+    if p:
+        print(f"{title} accuracy: {average}")
+        print(f"{title} f1 scores: {f1_score}")
+        print()
+    return f1_score, average
 
 strict_test_labels, relaxed_test_labels = getClassLabels("./data/CIRCA/circa-data-test.tsv")
 strict_dev_labels, relaxed_dev_labels = getClassLabels("./data/CIRCA/circa-data-dev.tsv")
@@ -54,3 +57,25 @@ printStatistics(strict_test_labels, pred_labels, "test strict")
 printStatistics(strict_dev_labels, pred_labels, "dev strict")
 printStatistics(relaxed_test_labels, pred_labels, "test relaxed")
 printStatistics(relaxed_dev_labels, pred_labels, "dev relaxed")
+
+strict_average_list = []
+relaxed_average_list = []
+for i in range(10):
+    strict_test_labels, relaxed_test_labels = getClassLabels("./data/CIRCA/unmatched/scenario" + str(i) + "/circa-data-test.tsv")
+    pred_labels = torch.zeroes(strict_test_labels.shape)
+    _, strict_average = printStatistics(pred_labels, strict_test_labels, p=False)
+    _, relaxed_average = printStatistics(pred_labels, relaxed_test_labels, p=False)
+    
+print("strict unmatched statistics")
+print(f"mean: {sum(strict_average_list) / len(strict_average_list)}")
+print(f"mean: {statistics.pstdev(strict_average_list)}")
+print(f"max : {max(strict_average_list)}")
+print(f"min : {min(strict_average_list)}")
+print()
+
+print("relaxed unmatched statistics")
+print(f"mean: {sum(relaxed_average_list) / len(relaxed_average_list)}")
+print(f"mean: {statistics.pstdev(relaxed_average_list)}")
+print(f"max : {max(relaxed_average_list)}")
+print(f"min : {min(relaxed_average_list)}")
+print()

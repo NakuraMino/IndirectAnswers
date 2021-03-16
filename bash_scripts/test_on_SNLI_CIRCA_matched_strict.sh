ca4db04a41a39215138497b09785bcfe4ad448fd
Andressa Chan <ac426@attu3.cs.washington.edu>
2021-03-15 22:21:12 -0700

    testing snli scripts

---
+++ bash_scripts/test_on_SNLI_CIRCA_matched_strict.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+## File for testing on the matched, relaxed dataset
+
+BASE_DIR="/home/azureuser/IndirectAnswers"
+
+cd "$BASE_DIR"
+
+echo "Testing on SNLI (pretrained) CIRCA with matched, relaxed labels..."
+
+# Evaluate on test dataset
+python testing.py \
+    --test_data "$BASE_DIR/data/CIRCA/circa-data-test.tsv" \
+    --model_type "$BASE_DIR/models/SNLI_CIRCA_BERT_matched_strict_e3_lr2e-5_b32" \
+    --dataset_type "CIRCA" \
+    --batch_size "32" \
+    --num_labels "9" \
+    --output_path "$BASE_DIR/results/SNLI-test-results.tsv"
+
+    # Evaluate on dev dataset
+python testing.py \
+    --test_data "$BASE_DIR/data/CIRCA/circa-data-dev.tsv" \
+    --model_type "$BASE_DIR/models/SNLI_CIRCA_BERT_matched_strict_e3_lr2e-5_b32" \
+    --dataset_type "CIRCA" \
+    --batch_size "32" \
+    --num_labels "9" \
+    --output_path "$BASE_DIR/results/SNLI-dev-results.tsv"
+
+echo "Finished testing SNLI (pretrained) CIRCA with matched, relaxed labels..."
---
+++ bash_scripts/test_on_SNLI_CIRCA_unmatched_strict.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## File for testing on the unmatched, strict dataset
+
+BASE_DIR="/home/azureuser/IndirectAnswers"  # IndirectAnswers directory
+UNMATCHED_DIR="$BASE_DIR/data/CIRCA/unmatched/scenario"
+
+cd "$BASE_DIR"
+
+ScenarioNum="0 1 2 3 4 5 6 7 8 9"
+
+echo "Testing on SNLI (pretrained) CIRCA with unmatched, strict labels..."
+
+lr="2e-5"
+for j in $ScenarioNum; do # 10 iterations, leaving a different scenario out each time
+    echo "STARTING... Mode: $mode, Learning rate: $lr, Scenario: $j"
+    echo "evaluating on test dataset"
+    # Evaluate on test dataset
+    python testing.py \
+    --test_data "${UNMATCHED_DIR}${j}/circa-data-test.tsv" \
+    --model_type "$BASE_DIR/models/SNLI_CIRCA_BERT_unmatched_strict_s${j}_e3_lr${lr}_b32" \
+    --dataset_type "CIRCA" \
+    --batch_size "32" \
+    --num_labels "9" \
+    --output_path "$BASE_DIR/results/SNLI-test-results.tsv"
+    
+    echo "evaluating on dev dataset"
+    # Evaluate on dev dataset
+    python testing.py \
+    --test_data "${UNMATCHED_DIR}${j}/circa-data-dev.tsv" \
+    --model_type "$BASE_DIR/models/SNLI_CIRCA_BERT_unmatched_strict_s${j}_e3_lr${lr}_b32" \
+    --dataset_type "CIRCA" \
+    --batch_size "32" \
+    --num_labels "9" \
+    --output_path "$BASE_DIR/results/SNLI-dev-results.tsv"
+done
+
+echo "Finished testing SNLI (pretrained) CIRCA with unmatched, strict labels..."
